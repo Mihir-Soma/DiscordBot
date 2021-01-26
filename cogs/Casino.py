2da5503133968dac8d3a5081a71ba11230892488
Mihir-Soma <70354282+Mihir-Soma@users.noreply.github.com>
2021-01-25 22:13:03 -0800

    added features to Casino
    -created command group for "bookie": "startBet", "placeBet", "closeBet"

--- cogs/Casino.py
+++ cogs/Casino.py
@@ -1,18 +1,18 @@
 import discord
 import time
 import asyncio
+import random
 import json
 import os
 from discord.ext import commands
 
+active = False
+bets = {}    
 
 class Casino(commands.Cog):
-    active = False
-    bets = []
-
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.command()
     async def register(self,ctx):
         with open("users.json", "r") as f:
@@ -23,40 +23,54 @@ class Casino(commands.Cog):
             users[authorid] = {"username":ctx.author.name, "nickname":ctx.author.display_name, "guap":10}
             with open("users.json", "w") as f:
                 json.dump(users, f)
-            await ctx.send("*thank you for registering, " + "<@!" + authorid + ">!*")
+            await ctx.send("*thank you for registering, " + ctx.author.mention + "!")
         else:
             await ctx.send("*you are already registered!*")
-
-    @commands.command()
-    async def bookie(self, ctx):
-        '''
-        users = {}
-        active = True
-        await ctx.send("*Place your bets!*")
-        '''
 
     @commands.command()
     async def balance(self,ctx):
         with open("users.json", "r") as f:
             users = json.load(f)
         authorid = str(ctx.author.id)
-        await ctx.send("<@!" + authorid + '>*you have ₲'+ str(users[authorid]["guap"])+"*")
+        await ctx.send(ctx.author.mention + '* you have ₲'+ str(users[authorid]["guap"])+"*")
 
-    @commands.command()
-    async def bet(self,ctx,amount: int):
+    @commands.group()
+    async def bookie(self, ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send("*invalid bookie command*")
+         
+    @bookie.command()
+    async def startBet(self,ctx, outcome1: str, outcome2: str):
+        await ctx.send("*place your bets! " + outcome1 + " or " + outcome2 +"?*")
+        active = True
+        await ctx.send(str(active))
+
+    @bookie.command()
+    async def placeBet(self,ctx, amount: int, onChoice: str):
         with open("users.json", "r") as f:
             users = json.load(f)
         authorid=str(ctx.author.id)
+        await ctx.send(str(active))
+        if active == True:
+            if amount <= users[authorid]["guap"] and amount > 0:
+                await ctx.send(ctx.author.mention + ' *your bet is ' + str(amount) + "*")
+                users[authorid]["guap"]-=amount
+                await ctx.send(users[authorid]["guap"])
+                with open("users.json", "w") as f:
+                    json.dump(users, f)
+                bets[authorid] = {onChoice: amount}
+            elif amount > users[authorid]["guap"]:
+                await ctx.send("*you don't have enough Guap*")
+            else:
+                await ctx.send("*invalid bet*")
+        elif active == False:
+            await ctx.send("*nothing to bet on!*")
 
-        if amount <= users[authorid]["guap"] and amount > 0:
-            await ctx.send("<@!" + authorid + '>*, your bet is ' + str(amount) + "*")
-            users[authorid]["guap"]-=amount
-            await ctx.send(users[authorid]["guap"])
-            with open("users.json", "w") as f:
-                json.dump(users, f)
-        else:
-            await ctx.send("*you don't have enough Guap*")
-        
+    @bookie.command()
+    async def closeBet(self,ctx):
+        await ctx.send("*bets can no longer be placed*")
+        active = False
+
 
 def setup(bot):
     bot.add_cog(Casino(bot))
